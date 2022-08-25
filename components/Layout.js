b73bd69ffa5298dcf25da8bbf4ad69d230cee605
Rashed Ahmed <rashed.ahmed@email.com>
2022-08-25 13:08:14 +0600

    testCommit

---
+++ components/Layout.js
@@ -0,0 +1,5 @@
+const Layout = ({ children }) => {
+	return <div className='container'>{children}</div>
+}
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+import Layout from '../components/Layout'
+
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<Layout>
+			<h1>Home</h1>
+			<h2>Home</h2>
+			<h3>Home</h3>
+			<h4>Home</h4>
+			<h5>Home</h5>
+			<h6>Home</h6>
+			<p>Home</p>
+			<p style={{ fontSize: '16px' }}>Home</p>
+		</Layout>
+	)
 }
 export default Home
