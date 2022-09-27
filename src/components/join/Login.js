3cdc69bfe4a4735780cec2a4ba5b436048d418fb
Rashed Ahmed <rashed.ahmed@email.com>
2022-09-27 16:30:03 +0600

    validate login page

--- src/components/join/Login.js
+++ src/components/join/Login.js
@@ -1,7 +1,23 @@
 import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z.object({
+	email: z.string().min(1, { message: 'Email is required' }).email({ message: 'Must be a valid email' }),
+	password: z
+		.string()
+		.min(1, { message: 'Password must contain special symbols' })
+		.min(8, { message: 'Password must be 8 characters long' }),
+})
 
 const LoginForm = ({ login }) => {
-	const { register, handleSubmit } = useForm()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(schema),
+	})
 	const onSubmit = values => {
 		console.log(values)
 	}
@@ -9,18 +25,34 @@ const LoginForm = ({ login }) => {
 		<form className='content' onSubmit={handleSubmit(onSubmit)}>
 			<p className='title'>Provide credentials to {login ? 'login' : 'sign up'} to ex-man</p>
 
-			<div className='formControl'>
+			<div className={`formControl${errors.email ? ' error' : ''}`}>
 				<label>Email Address</label>
-				<input {...register('email')} type='email' placeholder='Email Address' />
+				<input
+					type='text'
+					placeholder='Email Address'
+					{...register('email')}
+					aria-invalid={errors.email ? 'true' : 'false'}
+				/>
+				{errors.email && (
+					<ul>
+						<li role='alert'>{errors.email.message}</li>
+					</ul>
+				)}
 			</div>
 
-			<div className='formControl'>
+			<div className={`formControl${errors.password ? ' error' : ''}`}>
 				<label>Password</label>
-				<input {...register('password')} type='password' placeholder='••••••••••••••••' />
-				<ul>
-					<li>Password must be 8 characters long</li>
-					<li>Password must contain special symbols</li>
-				</ul>
+				<input
+					type='password'
+					placeholder='••••••••••••••••'
+					{...register('password')}
+					aria-invalid={errors.password ? 'true' : 'false'}
+				/>
+				{errors.password && (
+					<ul>
+						<li role='alert'>{errors.password.message}</li>
+					</ul>
+				)}
 			</div>
 
 			<div className='formControl'>
