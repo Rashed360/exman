72513b78c2f35b17c6aa0673b470b623694b5328
Rashed Ahmed <rashed.ahmed@email.com>
2022-09-28 18:33:28 +0600

    added tRPC to _app component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
-import { ThemeContextProvider } from '../utils/ThemeContext'
 import '../styles/globals.css'
 import '../styles/index.css'
+import { ThemeContextProvider } from '../utils/ThemeContext'
+import { withTRPC } from '@trpc/next'
+import { loggerLink } from '@trpc/client/links/loggerLink'
+import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
+import superjson from 'superjson'
+import { url } from '../constants'
+import { trpc } from '../utils/trpc'
 
 function Index({ Component, pageProps }) {
 	return (
@@ -10,4 +16,35 @@ function Index({ Component, pageProps }) {
 	)
 }
 
-export default Index
+export default withTRPC({
+	config({ ctx }) {
+		const links = [
+			loggerLink(),
+			httpBatchLink({
+				maxBatchSize: 10,
+				url,
+			}),
+		]
+		return {
+			queryClientConfig: {
+				defaultOptions: {
+					queries: {
+						staleTime: 60,
+					},
+				},
+			},
+			headers() {
+				if (ctx?.req) {
+					return {
+						...ctx.req.headers,
+						'x-ssr': '1',
+					}
+				}
+				return {}
+			},
+			links,
+			transformer: superjson,
+		}
+	},
+	ssr: false,
+})(Index)
